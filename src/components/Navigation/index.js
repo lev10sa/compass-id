6e5fb907c033ff1b22c5f1cdeb3642accc1d6b93
uncover-id <uncover.all.86@gmail.com>
2024-04-23 08:07:48 +0700

    init

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,14 +1,65 @@
 // import dependencies
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 // create the main function
 const Navigation = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const images = [
+    {
+      url: "/assets/img/banner/c.jpg",
+      class: "",
+    },
+    {
+      url: "/assets/img/banner/d.jpg",
+      class: "",
+    },
+    {
+      url: "/assets/img/banner/e.jpg",
+      class: "",
+    },
+    {
+      url: "/assets/img/banner/a.jpg",
+      class: "",
+    },
+    {
+      url: "/assets/img/banner/b.jpg",
+      class: "",
+    },
+  ];
+
   // setting up useNavigate
   const navigate = useNavigate();
 
   const openMenu = () => {
     document.getElementById("sidenav").classList.toggle("active");
+  };
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentIndex((prev) => (prev + 1) % images.length);
+      document.getElementById("images").classList.remove("active");
+      document.getElementById("images").classList.add("active");
+    }, 15000);
+
+    return () => clearInterval(intervalId);
+  }, [images.length]);
+
+  const handleNext = () => {
+    setCurrentIndex((prev) =>
+      prev === images.length - 1 ? prev - images.length + 1 : prev + 1
+    );
+    document.getElementById("images").classList.remove("active");
+    document.getElementById("images").classList.add("active");
+  };
+
+  const handlePrev = () => {
+    setCurrentIndex((prev) =>
+      prev === 0 ? prev + images.length - 1 : prev - 1
+    );
+    document.getElementById("images").classList.remove("active");
+    document.getElementById("images").classList.add("active");
   };
 
   // display of the navbar
@@ -27,23 +78,56 @@ const Navigation = () => {
           <button onClick={() => navigate(`/events`)}>
             <i className="fas fa-calendar-alt"></i> <span>Event</span>
           </button>
+          <button onClick={() => navigate(`/events`)}>
+            <i className="fas fa-file-alt"></i> <span>Article</span>
+          </button>
+          <button onClick={() => navigate(`/`)}>
+            <i className="fas fa-book"></i> <span>Book</span>
+          </button>
+          <button onClick={() => navigate(`/`)}>
+            <i className="fas fa-question-circle"></i> <span>Help</span>
+          </button>
           <button className="menu" onClick={openMenu}>
             <i className="fas fa-bars"></i>
           </button>
         </div>
       </div>
-      <div className="section banner">
-        <img src="/assets/img/banner/a.jpg" alt="" />
+      <div id="banner" className="section banner">
+        <img
+          src={images[currentIndex].url}
+          alt={images[currentIndex].url}
+          className="animate__animated animate__fadeIn animate__slow active"
+          id="images"
+          key={currentIndex}
+        />
+        <button className="prev" onClick={handlePrev}>
+          &#10094;
+        </button>
+        <button className="next" onClick={handleNext}>
+          &#10095;
+        </button>
       </div>
-      <div className="sidenav-wrapper" id="sidenav">
+      <div
+        className="sidenav-wrapper animate__animated animate__slideInRight"
+        id="sidenav"
+      >
         <div className="plain" onClick={openMenu}></div>
         <div className="sidenav">
           <img src="/logo.png" alt="" />
           <button onClick={() => navigate(`/`)}>
-            <i className="fas fa-home"></i> <span>Home</span>
+            <span>Home</span> <i className="fas fa-home"></i>
           </button>
           <button onClick={() => navigate(`/events`)}>
-            <i className="fas fa-calendar-alt"></i> <span>Event</span>
+            <span>Event</span> <i className="fas fa-calendar-alt"></i>
+          </button>
+          <button onClick={() => navigate(`/events`)}>
+            <span>Article</span> <i className="fas fa-file-alt"></i>
+          </button>
+          <button onClick={() => navigate(`/events`)}>
+            <span>Book</span> <i className="fas fa-book"></i>
+          </button>
+          <button onClick={() => navigate(`/events`)}>
+            <span>FAQ</span> <i className="fas fa-question-circle"></i>
           </button>
         </div>
       </div>
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,44 +1,141 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const getGreeting = () => {
+    const hours = currentTime.getHours();
+
+    if (hours >= 5 && hours < 12) {
+      return (
+        <h4>
+          <i className="fas fa-cloud-sun"></i> Good Morning!
+        </h4>
+      );
+    } else if (hours >= 12 && hours < 18) {
+      return (
+        <h4>
+          <i className="fas fa-sun"></i> Good Afternoon!
+        </h4>
+      );
+    } else if (hours >= 18 && hours < 21) {
+      return (
+        <h4>
+          <i className="fas fa-cloud-moon"></i> Good Evening!
+        </h4>
+      );
+    } else if (hours >= 21 || hours < 5) {
+      return (
+        <h4>
+          <i className="fas fa-star-and-crescent"></i> Good Night!
+        </h4>
+      );
+    }
+  };
+
+  const greeting = getGreeting();
+
+  const books = [
+    {
+      url: "https://i.compasspub.com/userfiles/item/20231122132720_itm.png",
+    },
+    {
+      url: "https://i.compasspub.com/userfiles/item/20231212144942_itm.jpg",
+    },
+    {
+      url: "https://i.compasspub.com/userfiles/item/20231117142036_itm.png",
+    },
+    {
+      url: "https://i.compasspub.com/userfiles/item/20231212145035_itm.jpg",
+    },
+    {
+      url: "https://i.compasspub.com/userfiles/item/2023080994546_itm.png",
+    },
+    {
+      url: "https://i.compasspub.com/userfiles/item/2023102511058_itm.jpg",
+    },
+  ];
+
+  const icons = [
+    {
+      src: "fas fa-calendar-alt",
+      url: "/events",
+      label: "Event",
+    },
+    {
+      src: "fas fa-file-alt",
+      url: "/",
+      label: "Article",
+    },
+    {
+      src: "fas fa-book",
+      url: "/",
+      label: "Book",
+    },
+    {
+      src: "fas fa-question-circle",
+      url: "/",
+      label: "FAQ",
+    },
+  ];
+
   return (
     <>
       <div className="container">
-        <div className="recom">
+        <div className="panel">
           <div className="section headline">
-            <h4>Recommendation</h4>
+            {greeting}
+            <div className="section">
+              <input
+                type="text"
+                autoComplete="on"
+                className="input"
+                placeholder="Search anything..."
+              />
+            </div>
           </div>
-          <div className="section scrollList">
-            <img
-              src="https://i.compasspub.com/userfiles/item/20231122132720_itm.png"
-              alt=""
-            />
-            <img
-              src="https://i.compasspub.com/userfiles/item/20231117142036_itm.png"
-              alt=""
-            />
-            <img
-              src="https://i.compasspub.com/userfiles/item/2023080994546_itm.png"
-              alt=""
-            />
-            <img
-              src="https://i.compasspub.com/userfiles/item/2023102511058_itm.jpg"
-              alt=""
-            />
-            <img
-              src="https://i.compasspub.com/userfiles/item/20231212145035_itm.jpg"
-              alt=""
-            />
-            <img
-              src="https://i.compasspub.com/userfiles/item/20231212144942_itm.jpg"
-              alt=""
-            />
+          <div className="section icons">
+            {icons.map((icon, index) => (
+              <>
+                <button
+                  type="button"
+                  onClick={() => navigate(icon.url)}
+                  key={index}
+                >
+                  <span>
+                    <i className={icon.src}></i>
+                  </span>
+                  <label>{icon.label}</label>
+                </button>
+              </>
+            ))}
           </div>
         </div>
-        <div className="section"></div>
-        <div className="about">
+        <div className="section recom">
           <div className="section headline">
-            <h4>Publication</h4>
+            <h5>Recommendation</h5>
+          </div>
+          <div className="section scrollList">
+            {books.map((book, index) => (
+              <>
+                <img src={book.url} alt="" key={index} />
+              </>
+            ))}
+          </div>
+        </div>
+        <div className="section about">
+          <div className="section headline">
+            <h5>Publication</h5>
           </div>
           <div className="section">
             <p>
@@ -53,7 +150,7 @@ function Home() {
         </div>
         <div className="section partner">
           <div className="section headline">
-            <h4>Partnership</h4>
+            <h5>Partnership</h5>
           </div>
           <div className="section thumbs">
             <img
