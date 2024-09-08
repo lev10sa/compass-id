04f85168e57722b8572a1694d1fe1440d7aba7e8
uncover-id <uncover.all.86@gmail.com>
2024-09-08 17:44:21 +0700

    init

--- public/sitemap.php
+++ public/sitemap.php
@@ -8,6 +8,9 @@ $bookData = json_decode($bookRes, true);
 $postIdUrl = "https://seg-server.vercel.app/api/posts/id";
 $postIdRes = file_get_contents($postIdUrl);
 $postIdData = json_decode($postIdRes, true);
+$postEnUrl = "https://seg-server.vercel.app/api/posts/en";
+$postEnRes = file_get_contents($postEnUrl);
+$postEnData = json_decode($postEnRes, true);
 $eventUrl = "https://seg-server.vercel.app/api/events";
 $eventRes = file_get_contents($eventUrl);
 $eventData = json_decode($eventRes, true);
@@ -29,6 +32,67 @@ foreach ($subdo as $sub) {
 
   $urlElement = $dom->createElement('url');
   $locElement = $dom->createElement('loc', "https://www.compasspubindonesia.com" . $sub);
+  $lastmodElement = $dom->createElement('lastmod', $date); // Update lastmod if needed
+  $changefreqElement = $dom->createElement('changefreq', 'daily');
+  $priorityElement = $dom->createElement('priority', '1.0');
+
+  $urlElement->appendChild($locElement);
+  $urlElement->appendChild($lastmodElement);
+  $urlElement->appendChild($changefreqElement);
+  $urlElement->appendChild($priorityElement);
+
+  $root->appendChild($urlElement);
+}
+
+// Process event data
+foreach ($eventData as $event) {
+  $urlElement = $dom->createElement('url');
+  $locElement = $dom->createElement('loc', "https://www.compasspubindonesia.com/event-join.php?id=" . $event["_id"]);
+  $lastmodElement = $dom->createElement('lastmod', $date); // Update lastmod if needed
+  $changefreqElement = $dom->createElement('changefreq', 'daily');
+  $priorityElement = $dom->createElement('priority', '1.0');
+
+  $urlElement->appendChild($locElement);
+  $urlElement->appendChild($lastmodElement);
+  $urlElement->appendChild($changefreqElement);
+  $urlElement->appendChild($priorityElement);
+
+  $root->appendChild($urlElement);
+}
+
+foreach ($postIdData as $postId) {
+  $urlElement = $dom->createElement('url');
+  $locElement = $dom->createElement('loc', "https://www.compasspubindonesia.com/post-view.php?lang=id&amp;id=" . $postId["_id"]);
+  $lastmodElement = $dom->createElement('lastmod', $date); // Update lastmod if needed
+  $changefreqElement = $dom->createElement('changefreq', 'daily');
+  $priorityElement = $dom->createElement('priority', '1.0');
+
+  $urlElement->appendChild($locElement);
+  $urlElement->appendChild($lastmodElement);
+  $urlElement->appendChild($changefreqElement);
+  $urlElement->appendChild($priorityElement);
+
+  $root->appendChild($urlElement);
+}
+
+foreach ($postEnData as $postEn) {
+  $urlElement = $dom->createElement('url');
+  $locElement = $dom->createElement('loc', "https://www.compasspubindonesia.com/post-view.php?lang=en&amp;id=" . $postEn["_id"]);
+  $lastmodElement = $dom->createElement('lastmod', $date); // Update lastmod if needed
+  $changefreqElement = $dom->createElement('changefreq', 'daily');
+  $priorityElement = $dom->createElement('priority', '1.0');
+
+  $urlElement->appendChild($locElement);
+  $urlElement->appendChild($lastmodElement);
+  $urlElement->appendChild($changefreqElement);
+  $urlElement->appendChild($priorityElement);
+
+  $root->appendChild($urlElement);
+}
+
+foreach ($bookData as $book) {
+  $urlElement = $dom->createElement('url');
+  $locElement = $dom->createElement('loc', "https://www.compasspubindonesia.com/book-view.php?id=" . $book["_id"]);
   $lastmodElement = $dom->createElement('lastmod', $date); // Update lastmod if needed
   $changefreqElement = $dom->createElement('changefreq', 'daily');
   $priorityElement = $dom->createElement('priority', '1.0');
@@ -72,6 +136,21 @@ foreach ($postIdData as $postId) {
   $root->appendChild($urlElement);
 }
 
+foreach ($postEnData as $postEn) {
+  $urlElement = $dom->createElement('url');
+  $locElement = $dom->createElement('loc', "https://www.compasspubindonesia.com/post-view/en/" . $postEn["_id"]);
+  $lastmodElement = $dom->createElement('lastmod', $date); // Update lastmod if needed
+  $changefreqElement = $dom->createElement('changefreq', 'daily');
+  $priorityElement = $dom->createElement('priority', '1.0');
+
+  $urlElement->appendChild($locElement);
+  $urlElement->appendChild($lastmodElement);
+  $urlElement->appendChild($changefreqElement);
+  $urlElement->appendChild($priorityElement);
+
+  $root->appendChild($urlElement);
+}
+
 foreach ($bookData as $book) {
   $urlElement = $dom->createElement('url');
   $locElement = $dom->createElement('loc', "https://www.compasspubindonesia.com/book-view/" . $book["_id"]);
@@ -88,4 +167,6 @@ foreach ($bookData as $book) {
 }
 
 // Save formatted XML document
-$dom->save('sitemap.xml');
+$doneit = $dom->save('sitemap.xml');
+
+$doneit ? header("location: sitemap.xml") : print "failed";
