a9343c4baf88408b2a6938b1e508424add2616c9
uncover-id <uncover.all.86@gmail.com>
2024-11-24 19:47:38 +0700

    init

--- public/forum/index.php
+++ public/forum/index.php
@@ -2,7 +2,7 @@
 
 include "./connection.php";
 
-if (ISSET($_SESSION['s_un']) && ISSET($_SESSION['s_pw'])) {
+if (ISSET($_SESSION['s_em']) && ISSET($_SESSION['s_pw'])) {
     header('location: ./home.php');
 } else {
     header('location: ./login.php');
