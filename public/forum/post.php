77265f82d7e67d4e57eadf673681c836a101c683
uncover-id <uncover.all.86@gmail.com>
2024-11-28 09:12:31 +0700

    init

--- public/forum/post.php
+++ public/forum/post.php
@@ -82,8 +82,8 @@ if (isset($_GET['post_id'])) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Forum | Compass Publishing Indonesia</title>
         <link rel="icon" href="https://compasspubindonesia.com/favicon.ico" type="image/x-icon">
-        <link rel="stylesheet" href="https://compasspubindonesia.com/login.css">
-        <link rel="stylesheet" href="https://compasspubindonesia.com/home.css">
+        <link rel="stylesheet" href="https://compasspubindonesia.com/forum/login.css">
+        <link rel="stylesheet" href="https://compasspubindonesia.com/forum/home.css">
         <link rel="stylesheet" href="./comment.css">
         <link
             rel="stylesheet"
