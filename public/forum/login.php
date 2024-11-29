dd653e7127fcaa9e1e8eb0a9bd6ff935875fadc0
uncover-id <uncover.all.86@gmail.com>
2024-11-29 11:40:15 +0700

    init

--- public/forum/login.php
+++ public/forum/login.php
@@ -41,7 +41,7 @@ if (isset($_SESSION['s_em']) && isset($_SESSION['s_pw'])) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Forum | Compass Publishing Indonesia</title>
         <link rel="icon" href="https://compasspubindonesia.com/favicon.ico" type="image/x-icon">
-        <link rel="stylesheet" href="./login.css">
+        <link rel="stylesheet" href="https://compasspubindonesia.com/login.css">
         <link
             rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
